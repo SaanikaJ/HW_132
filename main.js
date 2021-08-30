ffc9d85288cc8ef5744d55ae4ee3c4d5a8e257de
SaanikaJ <74892925+SaanikaJ@users.noreply.github.com>
2021-08-30 14:46:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function desk_room(){
+    window.location = "desk.html";
+}
+
+function flower_room(){
+    window.location = "flower.html";
+}
+
+function mirror_room(){
+    window.location = "mirror.html";
+}
+
+function curtain_room(){
+    window.location = "curtain.html";
+}
+
+function bookshelf_room(){
+    window.location = "bookshelf.html";
+}
+
+function back(){
+    window.location = "index.html";
+} 
